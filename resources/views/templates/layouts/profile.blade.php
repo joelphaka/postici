9296de7b3f6e32ba7f3af348f79b0a14b7810d52
Jooël Phaka <joelzr02@gmail.com>
2019-01-14 20:59:25 +0200

    Refactoring and added JQuery UI.

--- resources/views/templates/layouts/profile.blade.php
+++ resources/views/templates/layouts/profile.blade.php
@@ -7,20 +7,36 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>
-        @yield('pageTitle')
-    </title>
+    <title>@yield('pageTitle')</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="{{ url('/assets/css/bootstrap.css') }}" rel="stylesheet">
 
+    <!-- JQuery UI CSS -->
+    <link href="{{ url('/assets/jquery-ui/jquery-ui.min.css') }}" rel="stylesheet">
+    <link href="{{ url('/assets/jquery-ui/jquery-ui.structure.min.css') }}" rel="stylesheet">
+    <link href="{{ url('/assets/jquery-ui/jquery-ui.theme.min.css') }}" rel="stylesheet">
+
     <!-- Custom CSS -->
     <link href="{{ url('/assets/css/main.css') }}" rel="stylesheet">
     <link href="{{ url('/assets/css/styles.css') }}" rel="stylesheet">
+
     @yield('styles')
 
+    <!-- Custom Fonts -->
+    <link href="{{ url('/assets/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+
     <!-- jQuery -->
-    <script src="{{ url('/assets/js/jquery.js') }}"></script>
+    <script src="{{ url('/assets/js/jquery.min.js') }}"></script>
+    <script src="{{ url('/assets/jquery-ui/jquery-ui.min.js') }}"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 <body>
     @include('templates.partials.nav-profile')
