edd9425cc54726dd449442b99c373cb005154fa7
Joël Phaka <joelzr02@gmail.com>
2019-01-14 12:01:21 +0200

    Create followers_table_integrity_trigger.sql
    This script should be executed after migrations. This enforces the integrity on the followers table by preventing a user from following themselves. For example user_id=1 and follower_id=1 will be invalid,

---
+++ database/sql/followers_table_integrity_trigger.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2019 at 09:37 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `postici`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Triggers `followers`
+--
+DROP TRIGGER IF EXISTS `enforce_integrity`;
+DELIMITER $$
+CREATE TRIGGER `enforce_integrity` BEFORE INSERT ON `followers` FOR EACH ROW BEGIN
+	IF (NEW.user_id = NEW.follower_id) THEN
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Data integrity failed. followers.user_id cannot equal followers.follower_id';
+    END IF;
+END
+$$
+DELIMITER ;
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
